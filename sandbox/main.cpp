b143f11adb642bbeb992051801ba3f220ca33881
tim <timothee.ewart@epfl.ch>
2016-07-25 15:24:20 +0200

    new bench

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -14,45 +14,71 @@
 
 namespace queue{
 
+    static __inline__ unsigned long long rdtsc(void){
+        unsigned hi, lo;
+        __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+        return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32   );
+    }
+
     template<container q>
     double benchmark_queue_push(int size){
         std::default_random_engine generator;
         std::uniform_real_distribution<double> distribution(0.0,1.0);
-        std::chrono::duration<double> time(0);
+        unsigned long long int t1(0),t2(0),time(0);
 
         for(int j=0; j<10;++j){
             typename helper_type<q>::queue_type queue;
-            auto t1 = std::chrono::system_clock::now();
+            t1 = rdtsc();
                 for(int i = 0; i < size ; ++i)
                     queue.push(distribution(generator));
-            auto t2 = std::chrono::system_clock::now();
-            time += std::chrono::duration<double>(t2 - t1);
+            t2 = rdtsc();
+            time += (t2 - t1);
         }
 
-        return time.count()*0.1;
+        return time*0.1;
     }
 
     template<container q>
-    double benchmark_queue_pop(int size){
+    double benchmark_queue_push_one(int size){
+        unsigned long long int t1(0),t2(0),time(0);
         std::default_random_engine generator;
         std::uniform_real_distribution<double> distribution(0.0,1.0);
-        std::chrono::duration<double> time(0);
 
         for(int j=0; j<10;++j){
             typename helper_type<q>::queue_type queue;
             for(int i = 0; i < size ; ++i)
                 queue.push(distribution(generator));
 
-            auto t1 = std::chrono::system_clock::now();
+            t1 = rdtsc();
+            queue.push(distribution(generator));
+            t2 = rdtsc();
+            time += (t2 - t1);
+        }
+
+        return time*0.1;
+    }
+
+    template<container q>
+    double benchmark_queue_pop(int size){
+        std::default_random_engine generator;
+        std::uniform_real_distribution<double> distribution(0.0,1.0);
+        unsigned long long int t1(0),t2(0),time(0);
+
+        for(int j=0; j<10;++j){
+            typename helper_type<q>::queue_type queue;
+            for(int i = 0; i < size ; ++i)
+                queue.push(distribution(generator));
+
+            t1 = rdtsc();
 
             while(!queue.empty())
                 queue.pop();
 
-            auto t2 = std::chrono::system_clock::now();
+            t2 = rdtsc();
 
-            time += std::chrono::duration<double>(t2 - t1);
+            time += (t2 - t1);
         }
-        return  time.count()*0.1;
+        return  time*0.1;
     }
 
 
@@ -74,6 +100,26 @@ namespace queue{
         }
         std::fstream out;
         out.open("push.csv",std::fstream::out);
+        std::copy(res.begin(),res.end(), std::ostream_iterator<std::string>(out));
+    }
+
+
+    void benchmark_push_one(int iteration){
+        int size(100000);
+        std::list<std::string> res;
+        res.push_back("#elements,std::priority_queue,mh_splay,boost::binomial_heap,boost::fibonacci_heap,boost::pairing_heap,boost::skew_heap \n");
+
+        std::string bench = std::to_string(size) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::priority_queue>(size)) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::spltree>(size)) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::binomial_heap>(size)) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::fibonacci_heap>(size)) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::pairing_heap>(size)) + ",";
+        bench += std::to_string(queue::benchmark_queue_push_one<container::skew_heap>(size)) + "\n";
+        res.push_back(bench);
+
+        std::fstream out;
+        out.open("push_one.csv",std::fstream::out);
         std::copy(res.begin(),res.end(), std::ostream_iterator<std::string>(out));
     }
 
@@ -104,6 +150,7 @@ int main(int argc, char* argv[]){
     int iteration = std::atoi(argv[1]);
 
     queue::benchmark_push(iteration);
+    queue::benchmark_push_one(iteration);
     queue::benchmark_pop(iteration);
 
     return 0;
