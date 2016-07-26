2c75558d5253946988597162233c83b99b70e075
tim <timothee.ewart@epfl.ch>
2016-07-26 16:06:09 +0200

    the benchmarks is now c++11

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -11,168 +11,122 @@
 
 #include "tqueue.hpp"
 #include "trait.h"
+#include "timer_asm.h"
 
 namespace queue{
-#if defined(__x86_64__)
-    static __inline__ unsigned long long rdtsc(void){
-        unsigned hi, lo;
-        __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-        return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32   );
-    }
-#elif defined(__powerpc__)
-    static __inline__ unsigned long long rdtsc(void)
-    {
-        unsigned long long int result=0;
-        unsigned long int upper, lower,tmp;
-        __asm__ volatile(
-                      "0:                  \n"
-                      "\tmftbu   %0           \n"
-                      "\tmftb    %1           \n"
-                      "\tmftbu   %2           \n"
-                      "\tcmpw    %2,%0        \n"
-                      "\tbne     0b         \n"
-                      : "=r"(upper),"=r"(lower),"=r"(tmp)
-                      );
-        result = upper;
-        result = result<<32;
-        result = result|lower;
 
-        return(result);
-    }
-#endif
+    struct push_helper{
+        template<class T>
+        static double benchmark(int size){
+            using value_type = typename T::value_type;
+            std::default_random_engine generator;
+            std::uniform_real_distribution<double> distribution(0.0,1.0);
+            unsigned long long int t1(0),t2(0),time(0);
 
-    template<container q>
-    double benchmark_queue_push(int size){
-        std::default_random_engine generator;
-        std::uniform_real_distribution<double> distribution(0.0,1.0);
-        unsigned long long int t1(0),t2(0),time(0);
+            for(int j=0; j<10;++j){
+                value_type queue;
+                t1 = rdtsc();
+                    for(int i = 0; i < size ; ++i)
+                        queue.push(distribution(generator));
+                t2 = rdtsc();
+                time += (t2 - t1);
+            }
 
-        for(int j=0; j<10;++j){
-            typename helper_type<q>::queue_type queue;
-            t1 = rdtsc();
+            return time*0.1;
+        }
+
+        constexpr static auto name = "push";
+    };
+
+    struct pop_helper{
+        template<class T>
+        static double benchmark(int size){
+            using value_type = typename T::value_type;
+            std::default_random_engine generator;
+            std::uniform_real_distribution<double> distribution(0.0,1.0);
+            unsigned long long int t1(0),t2(0),time(0);
+
+            for(int j=0; j<10;++j){
+                value_type queue;
                 for(int i = 0; i < size ; ++i)
                     queue.push(distribution(generator));
-            t2 = rdtsc();
-            time += (t2 - t1);
+
+                t1 = rdtsc();
+                while(!queue.empty())
+                    queue.pop();
+                t2 = rdtsc();
+
+                time += (t2 - t1);
+            }
+            return  time*0.1;
         }
 
-        return time*0.1;
-    }
+        constexpr static auto name = "pop";
+    };
 
-    template<container q>
-    double benchmark_queue_push_one(int size){
-        unsigned long long int t1(0),t2(0),time(0);
-        std::default_random_engine generator;
-        std::uniform_real_distribution<double> distribution(0.0,1.0);
+    struct push_one_helper{
+            template<class T>
+            static double benchmark(int size){
+            using value_type = typename T::value_type;
+            unsigned long long int t1(0),t2(0),time(0);
+            std::default_random_engine generator;
+            std::uniform_real_distribution<double> distribution(0.0,1.0);
 
-        for(int j=0; j<10;++j){
-            typename helper_type<q>::queue_type queue;
-            for(int i = 0; i < size ; ++i)
+            for(int j=0; j<10;++j){
+                value_type queue;
+                for(int i = 0; i < size ; ++i)
+                    queue.push(distribution(generator));
+
+                t1 = rdtsc();
                 queue.push(distribution(generator));
+                t2 = rdtsc();
+                time += (t2 - t1);
+            }
 
-            t1 = rdtsc();
-            queue.push(distribution(generator));
-            t2 = rdtsc();
-            time += (t2 - t1);
+            return time*0.1;
         }
 
-        return time*0.1;
-    }
+        constexpr static auto name = "push_one";
+    };
 
-    template<container q>
-    double benchmark_queue_pop(int size){
-        std::default_random_engine generator;
-        std::uniform_real_distribution<double> distribution(0.0,1.0);
-        unsigned long long int t1(0),t2(0),time(0);
-
-        for(int j=0; j<10;++j){
-            typename helper_type<q>::queue_type queue;
-            for(int i = 0; i < size ; ++i)
-                queue.push(distribution(generator));
-
-            t1 = rdtsc();
-
-            while(!queue.empty())
-                queue.pop();
-
-            t2 = rdtsc();
-
-            time += (t2 - t1);
-        }
-        return  time*0.1;
-    }
-
-
-    void benchmark_push(int iteration){
+    template<class F, class ...T>
+    void benchmark(int iteration){
         int size(1);
         std::list<std::string> res;
-        res.push_back("#elements,std::priority_queue,mh_splay,boost::binomial_heap,boost::fibonacci_heap,boost::pairing_heap,boost::skew_heap \n");
+
+        res.push_back("#elements,"+name_helper<T...>::name()+"\n");
 
         for(int i=1; i< iteration; ++i){
-            std::string bench = std::to_string(size) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::priority_queue>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::spltree>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::binomial_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::fibonacci_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::pairing_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_push<container::skew_heap>(size)) + "\n";
-            res.push_back(bench);
+            std::string results = std::to_string(size) + ",";
+            results += benchmark_helper<F,T...>::benchmark(size)+"\n";
+            res.push_back(results);
             size<<=1; // 1,2,4,8 ....
         }
+        //start IO
         std::fstream out;
-        out.open("push.csv",std::fstream::out);
-        std::copy(res.begin(),res.end(), std::ostream_iterator<std::string>(out));
-    }
-
-
-    void benchmark_push_one(int iteration){
-        int size(100000);
-        std::list<std::string> res;
-        res.push_back("#elements,std::priority_queue,mh_splay,boost::binomial_heap,boost::fibonacci_heap,boost::pairing_heap,boost::skew_heap \n");
-
-        std::string bench = std::to_string(size) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::priority_queue>(size)) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::spltree>(size)) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::binomial_heap>(size)) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::fibonacci_heap>(size)) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::pairing_heap>(size)) + ",";
-        bench += std::to_string(queue::benchmark_queue_push_one<container::skew_heap>(size)) + "\n";
-        res.push_back(bench);
-
-        std::fstream out;
-        out.open("push_one.csv",std::fstream::out);
-        std::copy(res.begin(),res.end(), std::ostream_iterator<std::string>(out));
-    }
-
-
-    void benchmark_pop(int iteration){
-        int size(1);
-        std::list<std::string> res;
-        res.push_back("#elements,std::priority_queue,mh_splay,boost::binomial_heap,boost::fibonacci_heap,boost::pairing_heap,boost::skew_heap \n");
-
-        for(int i=1; i< iteration; ++i){
-            std::string bench = std::to_string(size) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::priority_queue>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::spltree>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::binomial_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::fibonacci_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::pairing_heap>(size)) + ",";
-            bench += std::to_string(queue::benchmark_queue_pop<container::skew_heap>(size)) + "\n";
-            res.push_back(bench);
-            size<<=1; // 1,2,4,8 ....
-        }
-        std::fstream out;
-        out.open("pop.csv", std::fstream::out);
+        std::string name = std::string(F::name) + ".csv";
+        out.open(name,std::fstream::out);
         std::copy(res.begin(),res.end(), std::ostream_iterator<std::string>(out));
     }
 } // end name space
 
 int main(int argc, char* argv[]){
-    int iteration = std::atoi(argv[1]);
-
-    queue::benchmark_push(iteration);
-    queue::benchmark_push_one(iteration);
-    queue::benchmark_pop(iteration);
+    int size = std::atoi(argv[1]);
+    //queue types
+    using t0 = helper_type<priority_queue>;
+    using t1 = helper_type<spltree>;
+    using t2 = helper_type<binomial_heap>;
+    using t3 = helper_type<fibonacci_heap>;
+    using t4 = helper_type<pairing_heap>;
+    using t5 = helper_type<skew_heap>;
+    //bench types
+    using push = queue::push_helper;
+    using pop = queue::pop_helper;
+    using push_one = queue::push_one_helper;
+    //benchmarks
+    queue::benchmark<push,t0,t1,t2,t3,t4,t5>(size);
+    queue::benchmark<pop,t0,t1,t2,t3,t4,t5>(size);
+    queue::benchmark<push_one,t0,t1,t2,t3,t4,t5>(size);
 
     return 0;
 }
--- sandbox/trait.h
+++ sandbox/trait.h
@@ -18,37 +18,80 @@
 
 enum container {spltree, priority_queue,binomial_heap,fibonacci_heap,pairing_heap,skew_heap};
 
+//name helper printer
+template<class D, class... T>
+struct name_helper;
+
+template<class T>
+struct name_helper<T>{
+    static std::string name(){
+        return T::name;
+    }
+};
+
+template<class D, class ...T>
+struct name_helper{
+    static std::string name(){
+        return name_helper<D>::name()+","+name_helper<T...>::name();
+    }
+};
+
+//benchmark helper execution
+template<class F, class D, class... T>
+struct benchmark_helper;
+
+template<class F, class D>
+struct benchmark_helper<F,D>{
+    static std::string benchmark(int size){
+        return (std::to_string(F::template benchmark<D>(size)));
+    }
+};
+
+template<class F, class D, class ...T>
+struct benchmark_helper{
+    static std::string benchmark(int size){
+        return benchmark_helper<F,D>::benchmark(size)+","+benchmark_helper<F,T...>::benchmark(size);
+    }
+};
+
+
 template<container q>
 struct helper_type;
 
 template<>
 struct helper_type<priority_queue>{
-    typedef std::priority_queue<double,std::vector<double>,std::greater<double>> queue_type;
+    typedef std::priority_queue<double,std::vector<double>,std::greater<double>> value_type;
+    constexpr static auto name = "std::priority_queue";
 };
 
 template<>
 struct helper_type<spltree>{
-    typedef sptq::TQueue<double, std::greater<double>> queue_type;
+    typedef sptq::TQueue<double, std::greater<double>> value_type;
+    constexpr static auto name = "original_queue";
 };
 
 template<>
 struct helper_type<binomial_heap>{
-    typedef boost::heap::binomial_heap<double, boost::heap::compare<std::greater<double>>> queue_type;
+    typedef boost::heap::binomial_heap<double, boost::heap::compare<std::greater<double>>> value_type;
+    constexpr static auto name = "boost::binomial_heap";
 };
 
 template<>
 struct helper_type<fibonacci_heap>{
-    typedef boost::heap::fibonacci_heap<double, boost::heap::compare<std::greater<double>>> queue_type;
+    typedef boost::heap::fibonacci_heap<double, boost::heap::compare<std::greater<double>>> value_type;
+    constexpr static auto name = "boost::fibonacci_heap";
 };
 
 template<>
 struct helper_type<pairing_heap>{
-    typedef boost::heap::pairing_heap<double, boost::heap::compare<std::greater<double>>> queue_type;
+    typedef boost::heap::pairing_heap<double, boost::heap::compare<std::greater<double>>> value_type;
+    constexpr static auto name = "boost::pairing_heap";
 };
 
 template<>
 struct helper_type<skew_heap>{
-    typedef boost::heap::skew_heap<double, boost::heap::compare<std::greater<double>>> queue_type;
+    typedef boost::heap::skew_heap<double, boost::heap::compare<std::greater<double>>> value_type;
+    constexpr static auto name = "boost::skew_heap";
 };
 
 #endif /* trait_h */
