5480447b1a5a5cbe7a7c2ae6f37bdc0ce0d5da55
tim <timothee.ewart@epfl.ch>
2016-07-26 17:36:06 +0200

    travis again

--- src/tqueue.hpp
+++ src/tqueue.hpp
@@ -74,7 +74,7 @@ public:
     }
 
     inline value_type top(){
-        value_type tmp = value_type();
+        value_type tmp;
         if(!empty())
             tmp = sptq::sphead<T,Compare>(&q)->key();
         return tmp;
