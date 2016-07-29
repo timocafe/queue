11b7cd51c58483e13e4d88077c89bd21babb7c58
tim <timothee.ewart@epfl.ch>
2016-07-29 13:15:13 +0200

    update benchmarks core i5

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -18,7 +18,7 @@ namespace queue{
 
     struct mhines_bench_helper {
         template<class T>
-        static double benchmark(int size, int repetition = 2){
+        static double benchmark(int size, int repetition = 1){
             repetition=1;
             using value_type = typename T::value_type;
             std::default_random_engine generator;
@@ -29,8 +29,8 @@ namespace queue{
             const double max_time = 50.0;
 
             for(int j=0; j<repetition; ++j){
+                value_type queue;
                 for(auto t=0.0; t < max_time; t += dt){
-                    value_type queue;
                     t1 = rdtsc();
                     for(int i = 0; i < size ; ++i)
                         queue.push(t + distribution(generator));
@@ -51,18 +51,22 @@ namespace queue{
 
     struct push_helper{
         template<class T>
-        static double benchmark(int size, int repetition = 10){
+        static double benchmark(int size, int repetition = 5){
             using value_type = typename T::value_type;
             std::default_random_engine generator;
             std::uniform_real_distribution<double> distribution(0.0,1.0);
             unsigned long long int t1(0),t2(0),time(0);
+            value_type queue;
 
             for(int j=0; j<repetition; ++j){
-                value_type queue;
                 t1 = rdtsc();
                 for(int i = 0; i < size ; ++i)
                     queue.push(distribution(generator));
                 t2 = rdtsc();
+
+                while(!queue.empty())
+                    queue.pop();
+
                 time += (t2 - t1);
             }
             return time*1/static_cast<double>(repetition);
@@ -73,14 +77,14 @@ namespace queue{
 
     struct pop_helper{
         template<class T>
-        static double benchmark(int size, int repetition = 10){
+        static double benchmark(int size, int repetition = 5){
             using value_type = typename T::value_type;
             std::default_random_engine generator;
             std::uniform_real_distribution<double> distribution(0.0,1.0);
             unsigned long long int t1(0),t2(0),time(0);
+            value_type queue;
 
             for(int j=0; j<repetition; ++j){
-                value_type queue;
                 for(int i = 0; i < size ; ++i)
                     queue.push(distribution(generator));
 
@@ -104,16 +108,18 @@ namespace queue{
             unsigned long long int t1(0),t2(0),time(0);
             std::default_random_engine generator;
             std::uniform_real_distribution<double> distribution(0.0,1.0);
+            value_type queue;
+
+            for(int i = 0; i < size-1 ; ++i) // size-1 because to avoid realloc
+                queue.push(distribution(generator));
 
             for(int j=0; j<repetition; ++j){
-                value_type queue;
-                for(int i = 0; i < size ; ++i)
-                    queue.push(distribution(generator));
-
+                typename value_type::value_type value = distribution(generator);
                 t1 = rdtsc();
-                queue.push(distribution(generator));
+                queue.push(value);
                 t2 = rdtsc();
                 time += (t2 - t1);
+                queue.pop(); // keep the number of element constant
             }
             return time*1/static_cast<double>(repetition);
         }
@@ -140,28 +146,26 @@ namespace queue{
 } // end name space
 
 int main(int argc, char* argv[]){
-//    int iteration = std::atoi(argv[1]);
-//    //queue types
-//    using t0 = helper_type<priority_queue>;
-//    using t1 = helper_type<sptq_queue>;
-//    using t2 = helper_type<bin_queue>;
-//    using t3 = helper_type<binomial_heap>;
-//    using t4 = helper_type<fibonacci_heap>;
-//    using t5 = helper_type<pairing_heap>;
-//    using t6 = helper_type<skew_heap>;
-//    using t7 = helper_type<d_ary_heap>;
-//    //bench types
-//    using push = queue::push_helper;
-//    using pop = queue::pop_helper;
-//    using push_one = queue::push_one_helper;
-//    using mh = queue::mhines_bench_helper;
-//    //benchmarks
-//    queue::benchmark<push,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
-//    queue::benchmark<pop,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
-//    queue::benchmark<push_one,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
-//    queue::benchmark<push_one,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
-//    queue::benchmark<mh,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
-
+    int iteration = std::atoi(argv[1]);
+    //queue types
+    using t0 = helper_type<priority_queue>;
+    using t1 = helper_type<sptq_queue>;
+    using t2 = helper_type<bin_queue>;
+    using t3 = helper_type<binomial_heap>;
+    using t4 = helper_type<fibonacci_heap>;
+    using t5 = helper_type<pairing_heap>;
+    using t6 = helper_type<skew_heap>;
+    using t7 = helper_type<d_ary_heap>;
+    //bench types
+    using push = queue::push_helper;
+    using pop = queue::pop_helper;
+    using push_one = queue::push_one_helper;
+    using mh = queue::mhines_bench_helper;
+    //benchmarks
+    queue::benchmark<push,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
+    queue::benchmark<pop,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
+    queue::benchmark<push_one,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
+    queue::benchmark<mh,t0,t1,t2,t3,t4,t5,t6,t7>(iteration);
 
     return 0;
 }
