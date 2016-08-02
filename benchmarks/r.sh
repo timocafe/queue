8a4574076f4dc756f0771b7b13e5a4cf04b7fd65
tim <timothee.ewart@epfl.ch>
2016-08-02 13:21:19 +0200

    rebench all with new plot with limits test case

--- benchmarks/r.sh
+++ benchmarks/r.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -a
 
 for i in E2670 PowerA2 i5
 do
@@ -7,12 +7,15 @@ do
     case $i in
         E2670)
         sed -i -e "s/NAME/${i}-2.6[GHz]-GCC5.1/g" plot.txt
+        sed -i -e "s/FREQ/2.6*10**-9/g" plot.txt
         ;;
         PowerA2)
         sed -i -e "s/NAME/${i}-1.7[GHz]-GCC4.7/g" plot.txt
+        sed -i -e "s/FREQ/1.7*10**-9/g" plot.txt
         ;;
         i5)
         sed -i -e "s/NAME/${i}-2.5[GHz]-CLANG3.6/g" plot.txt
+        sed -i -e "s/FREQ/2.5*10**-9/g" plot.txt
         ;;
     esac
     gnuplot plot.txt
